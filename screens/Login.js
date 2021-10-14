8875f57143589b94829be6fe5ff36236d49d6985
derrickpennel0 <dadjei-mensah003@st.ug.edu.gh>
2021-10-14 18:40:30 +0000

    Initialize Work

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {View, Text, Image} from 'react-native'
+import {View, Text, Image, TouchableOpacity} from 'react-native'
+import {AntDesign} from '@expo/vector-icons'
 
 export default function Login(){
     return (
@@ -17,16 +18,41 @@ export default function Login(){
     source = {{url: "https://ychef.files.bbci.co.uk/1376x774/p07phq4b.jpg"}} /> 
      <Text style= {{ color : "rgba(0,0,0,0.6)", fontSize : 35}}> Welcome to</Text>
      <Text style = {{color: "black", fontSize: 40, fontWeight : "600",}}>Power Bike Shop</Text>
-     <TouchableOpacity style ={{
-         backgroundColor:"grey", 
+      
+          <View style = {{}}>
+      <TouchableOpacity style={{
+      backgroundColor:"rgb(224,224,224)", 
          padding : 10, 
          paddingHorizontal :60,
          marginTop: 20,
+         borderRadius : 10,
+         flexDirection : "row",
          }}
          >  
-     <Text style= {{fontSize : 20}}>Login with Gmail</Text>
+        
+         <AntDesign name="google" size={24} color="orange" />
+     <Text style= {{fontSize : 20, marginLeft:10}}>Login with Gmail</Text>
+     </TouchableOpacity>
+           </View>
+
+     <TouchableOpacity style ={{
+      backgroundColor:"black", 
+         padding : 10, 
+         paddingHorizontal :60,
+         marginTop: 20,
+         borderRadius : 10,
+         flexDirection : "row",
+         }}
+         >  
+         <AntDesign name="apple1" size={24} color="white" />
+     <Text style= {{fontSize : 20, marginLeft:10, color :"white"}}>Login with Apple</Text>
      </TouchableOpacity>
 
+     <View>
+     <TouchableOpacity>
+     <Text style={{marginTop:10}}> Not a member? <Text style={{color:"orange", fontSize:15 ,fontWeight:'bold'}}>Signup</Text></Text>
+     </TouchableOpacity>
+     </View>
     </View> 
      )
     
